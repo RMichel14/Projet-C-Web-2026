4f3bcb584f753a3206cf8b50cba2795366e4ed76
Raphaël Michel <raphaelmichel@outlook.fr>
2026-06-10 20:51:19 +0200

    Update calculs_trajectoire.h
    Mise à jour des structures (type parfois incohérent).
    Ajout de la fonction d'initialisation des trajectoires.

--- include/calculs_trajectoire.h
+++ include/calculs_trajectoire.h
@@ -16,16 +16,22 @@
 
 // Structure point
 typedef struct _point {
-    vector r; // position
-    vector v; // vitesse
-    int p;    // point (temps)
+    vector r;  // position
+    vector v;  // vitesse
+    int temps;
 } point;
+
+// Structure trajectoire
+typedef struct _trajectoire {
+    point point;
+    struct _trajectoire *nextpoint;
+} trajectoire;
 
 // Structure planète
 typedef struct _planete {
     char *nom;
     double masse;
-    double trajectoire;
+    trajectoire trajectoire;
     double perihelie;   // distance minimale planète/soleil
     double vitesseInitiale;
 } planete;
@@ -40,5 +46,12 @@ typedef struct _planete {
  * @param listePlanetes pointeur vers le tableau de la liste des planètes
  */
 void planeteTest(planete *listePlanetes);
+
+/**
+ * Initialisation trajectoire d'une planète.
+ * @param pl pointeur vers une planète.
+ */
+void initialiserTrajectoirePlanete(planete *pl);
+
 
 #endif
