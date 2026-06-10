f48d0ee696cb2a977598c8a220b1d302dec446ea
Raphaël Michel <raphaelmichel@outlook.fr>
2026-06-10 19:18:15 +0200

    Update calculs_trajectoire.h
    Ajout des signatures et autres besoins pour les calculs sur les vecteurs.

--- include/calculs_trajectoire.h
+++ include/calculs_trajectoire.h
@@ -4,8 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #define NB_PLANETES 8
+#define CARRE(x) ((x)*(x))
 
 
 /**********************************************
@@ -32,18 +34,29 @@ typedef struct _planete {
     double masse;
     double trajectoire;
     double perihelie;   // distance minimale planète/soleil
+    double vitesseInitiale;
 } planete;
 
 
 
 /**********************************************
- ** Fonctions mathématiques sur les vecteurs **
+ ****************** Vecteurs ******************
  **********************************************/
 
-// addition de deux vecteurs
-// soustraction de deux vecteurs
-// multiplication d'un vecteur par un scalaire
-// calcul de la norme d'un vecteur
+/**
+ * Permet de créer un nouveau vecteur
+ * @param x type : double
+ * @param y type : double
+ * @param z type : double
+ * @return pointeur vers un nouveau vecteur
+ */
+vector *nouveauVecteur(double x, double y, double z);
+
+/**
+ * Afficher vecteur
+ * @param vect vecteur | type : pointeur struct vector
+ */
+void afficherVecteur(vector *vect);
 
 /**
  * Fonction de test sur les vecteurs
@@ -51,6 +64,42 @@ typedef struct _planete {
  *             Ces fonctions seront appelées dans la fonction main puis commentées une fois les tests validés.
  */
 void vectorTest();
+
+
+/**********************************************
+ ** Fonctions mathématiques sur les vecteurs **
+ **********************************************/
+
+/**
+ * Addition de deux vecteurs
+ * @param vect1 premier vecteur | type : pointeur struct vector
+ * @param vect2 second vecteur | type : pointeur struct vector
+ * @return vector
+ */
+vector *addition(vector *vect1, vector *vect2);
+
+/**
+ * Soustraction de deux vecteurs
+ * @param vect1 premier vecteur | type : pointeur struct vector
+ * @param vect2 second vecteur | type : pointeur struct vector
+ * @return vector
+ */
+vector *soustraction(vector *vect1, vector *vect2);
+
+/**
+ * Multiplication d'un vecteur par un scalaire
+ * @param vect vecteur | type : pointeur struct vector
+ * @param number scalaire | type : int
+ * @return vector
+ */
+vector *multiplication(vector *vect, int number);
+
+/**
+ * Calcul de la norme d'un vecteur
+ * @param vect vecteur | type : pointeur struct vector
+ * @return double
+ */
+double norme(vector *vect);
 
 
 /**********************************************
