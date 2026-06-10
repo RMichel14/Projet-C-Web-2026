bcbe0de3e3b17396f3609b642801aaaaf0bd3f5a
Raphaël Michel <raphaelmichel@outlook.fr>
2026-06-10 15:20:46 +0200

    Create main.c
    Fichier principal du projet ; appel des tests unitaires.

---
+++ filesC/main.c
@@ -0,0 +1,13 @@
+#include "calculs_trajectoire.h"
+
+
+// Corps du programme
+int main() {
+    planete listePlanetes[NB_PLANETES];
+    
+    // Tests unitaires
+    printf("Hello world!\n");
+    planeteTest(listePlanetes);
+
+    return EXIT_SUCCESS;
+}
