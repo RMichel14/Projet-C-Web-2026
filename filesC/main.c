7f5b561e4f15f60b7c8bf7051b49b3b7d3217af2
Raphaël Michel <raphaelmichel@outlook.fr>
2026-06-10 20:49:58 +0200

    Update main.c
    Initialisation des données pour chaque planète ; statut : en production.

--- filesC/main.c
+++ filesC/main.c
@@ -1,15 +1,27 @@
 #include "calculs_trajectoire.h"
 #include "vecteurs.h"
+#include "lecture_ecriture.h"
 
 
 // Corps du programme
 int main() {
     planete listePlanetes[NB_PLANETES] = {0};
+
+    // lecture des données des planètes
+    lectureDonneesInitialesPlanetes("data/donnees_initiales_planetes.txt", listePlanetes);
     
+    for(int i = 0; i < NB_PLANETES; i++)
+        initialiserTrajectoirePlanete(&listePlanetes[i]);
+
+
+
     // Tests unitaires
     printf("Hello world!\n");
-    planeteTest(listePlanetes);
-    vectorTest();
+    // planeteTest(listePlanetes);
+    // vectorTest();
 
+    // libération de la mémoire
+    for (int i = 0; i < NB_PLANETES; i++)
+        free(listePlanetes[i].nom);
     return EXIT_SUCCESS;
 }
