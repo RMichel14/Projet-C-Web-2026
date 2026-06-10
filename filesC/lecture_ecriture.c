c6b99be748d499a21a81ebf90a0752c2c283fed6
Raphaël Michel <raphaelmichel@outlook.fr>
2026-06-10 17:14:44 +0200

    Update lecture_ecriture.c
    Améliorations & correction de bugs

--- filesC/lecture_ecriture.c
+++ filesC/lecture_ecriture.c
@@ -12,19 +12,19 @@ void lectureDonneesInitialesPlanetes(char *nomDuFichier, planete *listePlanetes)
     size_t len = 0;                                 // taille du buffer
     int count = 0;                                  // compteur du nombre de planètes pour parcourir et ne pas dépasser la taille du tableau
 
-    while (getline(&line, &len, monFichier) != -1) {
-        if (count >= NB_PLANETES) break;            // si le compteur de planètes dépasse le nombre de planète accepté dans le tableau -> stop
+    // si le compteur de planètes dépasse le nombre de planète accepté dans le tableau ou si la ligne est vide (résultat de getline(...) => -1) -> la boucle s'arrête
+    while (count < NB_PLANETES && getline(&line, &len, monFichier) != -1) {
 
         // Stockage temporaire des informations d'une planète
         char name[100];
         double masse;
         double perihelie;
 
-        if (sscanf(line, "%99s %lf %lf", name, &masse, &perihelie) == 3) {  // l'utilisation de %99s est pour éviter un dépassement dans le tableau 'name' de 100 caractères
+        if (sscanf(line, "%99s %le %le", name, &masse, &perihelie) == 3) {  // l'utilisation de %99s est pour éviter un dépassement dans le tableau 'name' de 100 caractères
             listePlanetes[count].nom = malloc(strlen(name) + 1);
 
             if (listePlanetes[count].nom != NULL) {
-                strcpy(listePlanetes[count].nom, name);
+                strcpy(listePlanetes[count].nom, name); // copie du tableau du nom de la planète dans la structure planete
 
                 listePlanetes[count].masse = masse;
                 listePlanetes[count].perihelie = perihelie;
