a5fa7e80b8b76b2a0b1764f199bad074ca5ca6e2
Raphaël Michel <raphaelmichel@outlook.fr>
2026-06-11 23:45:46 +0200

    Ecriture de la fonction d'export des données
    Signature et code des fonctions d'export des données au format JSON.

--- filesC/lecture_ecriture.c
+++ filesC/lecture_ecriture.c
@@ -1,4 +1,55 @@
 #include "lecture_ecriture.h"
+#include "calculs_trajectoire.h"
+
+void exporterTrajectoireJSON(FILE *fichier, trajectoire *traj, const int nb) {
+    trajectoire *courant = traj;
+
+    while (courant != NULL) {
+        fprintf(
+            fichier,
+            "[[%e,%e,%e], [%e,%e,%e], %d]",
+            courant->point.r.x, courant->point.r.y, courant->point.r.z,
+            courant->point.v.x, courant->point.v.y, courant->point.v.z,
+            courant->point.temps
+        );
+
+        if (courant->nextpoint != NULL)
+            fprintf(fichier, ", \n");
+        else
+            fprintf(fichier, "\n");
+
+        courant = courant->nextpoint;
+    }
+
+    if (nb == (NB_PLANETES - 1))
+        fprintf(fichier, "]");
+    else
+        fprintf(fichier, "],");
+}
+
+void exporterJSON(const char *nomFichier, planete *listePlanetes) {
+    FILE *monFichier = fopen(nomFichier, "w");     // création d'un pointeur et ouverture du fichier
+
+    if(monFichier == NULL) {                       // Si l'ouverture du fichier ne se passe pas bien
+        fprintf(stderr, "Impossible d'ouvrir %s\n", nomFichier);
+        return;
+    }
+
+    // tableau du nom des trajectoires
+    const char *nomTrajectoireTab[8] = {"earth-euler", "venus-euler", "mercure-euler", "mars-euler", "jupiter-euler", "saturne-euler", "uranus-euler", "neptune-euler"};
+
+    fprintf(monFichier, "{\n");
+
+    for (int i = 0; i < NB_PLANETES; i++) {
+        fprintf(monFichier, "\"%s\" : [\n", nomTrajectoireTab[i]);
+        exporterTrajectoireJSON(monFichier, &listePlanetes[i].trajectoire, i);
+    }
+
+    fprintf(monFichier, "\n}\n");
+
+    fclose(monFichier);
+}
+
 
 void lectureDonneesInitialesPlanetes(char *nomDuFichier, planete *listePlanetes) {
     FILE * monFichier = fopen(nomDuFichier, "r");   // creation d'un pointeur FILE pour le fichier & ouverture
@@ -16,7 +67,7 @@ void lectureDonneesInitialesPlanetes(char *nomDuFichier, planete *listePlanetes)
     while (count < NB_PLANETES && getline(&line, &len, monFichier) != -1) {
 
         // Stockage temporaire des informations d'une planète
-        char name[100]         = {0};
+        char   name[100]       = {0};
         double masse           = 0;
         double perihelie       = 0;
         double vitesseInitiale = 0;
--- include/lecture_ecriture.h
+++ include/lecture_ecriture.h
@@ -4,6 +4,13 @@
 #include "calculs_trajectoire.h"
 
 /**
+ * Fonction pour exporter les données calculées au format JSON.
+ * @param nomFichier chaine de caractères contenant le nom du fichier à enregistrer | type : char *
+ * @param listePlanetes pointeur vers la liste des planètes | type : planete *
+ */
+void exporterJSON(const char *nomFichier, planete *listePlanetes);
+
+/**
  * Fonction qui permet de lire ligne par ligne
  * @param nomDuFichier chemin vers le fichier cible
  * @param listePlanetes pointeur vers le tableau de la liste des planètes
